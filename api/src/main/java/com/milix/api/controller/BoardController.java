989f5058d2ae8585546a695d741f467ba77c2ed2
Milix-M <tujikou12dh@gmail.com>
2024-05-07 23:03:00 +0900

    add board update, delete endpoint

--- api/src/main/java/com/milix/api/controller/BoardController.java
+++ api/src/main/java/com/milix/api/controller/BoardController.java
@@ -1,6 +1,5 @@
 package com.milix.api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +10,12 @@ import com.milix.api.service.BoardService;
 import com.milix.api.service.dto.BoardDto;
 import com.milix.api.service.dto.HttpResponseDto;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class BoardController {
@@ -47,6 +47,29 @@ public class BoardController {
         BoardDto newBoard = boardService.createBoard(board);
         httpResponseDto.setHttpStatus(HttpStatus.OK);
         httpResponseDto.setResponseData(newBoard);
+        return httpResponseDto;
+    }
+
+    @PutMapping("/api/board/{boardId}")
+    public HttpResponseDto updateBoard(@PathVariable("boardId") String boardid, @RequestBody BoardDto board) {
+        var httpResponseDto = new HttpResponseDto();
+        board.setId(boardid);
+        BoardDto updatedBoard = boardService.updateBoard(board);
+        httpResponseDto.setHttpStatus(HttpStatus.OK);
+        httpResponseDto.setResponseData(updatedBoard);
+        return httpResponseDto;
+    }
+
+    @DeleteMapping("/api/board/{boardId}")
+    public HttpResponseDto deleteBoard(@PathVariable("boardId") String boardId) {
+        var httpResponseDto = new HttpResponseDto();
+        if (boardService.deleteBoardById(boardId)) {
+            httpResponseDto.setHttpStatus(HttpStatus.OK);
+            httpResponseDto.setMessage("delete ok.");
+        } else {
+            httpResponseDto.setHttpStatus(HttpStatus.BAD_REQUEST);
+            httpResponseDto.setMessage("bad request.");
+        }
         return httpResponseDto;
     }
 }
