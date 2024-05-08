37e314d769fd3602ffcd35ed1d29b226b314d295
Milix-M <tujikou12dh@gmail.com>
2024-05-08 15:25:48 +0900

    add types

---
+++ web/lib/types.ts
@@ -0,0 +1,11 @@
+export interface CommentType {
+  id?: number;
+  handle_name?: string;
+  comment_order_number?: number;
+  comment_text: string;
+}
+
+export interface BoardType {
+  id?: number;
+  board_name: string;
+}
