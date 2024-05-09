4d667c984f09a7bbd4a96e801e83b2d5f02750bb
Milix-M <tujikou12dh@gmail.com>
2024-05-09 16:32:24 +0900

    add api settings

---
+++ web/lib/axios.ts
@@ -0,0 +1,11 @@
+import Axios from 'axios'
+
+const axios = Axios.create({
+  withCredentials: true,
+  baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT as string,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export default axios
