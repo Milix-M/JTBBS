f54435ac8e81dcd4f70fff27443d6da5cd44535b
Milix-M <tujikou12dh@gmail.com>
2024-05-10 14:11:26 +0900

    update index.tsx

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,11 +1,28 @@
-import { Inter } from 'next/font/google'
+import BoardSelectBtn from "@/components/button/boardSelectBtn";
+import getLayout from "@/components/layouts/non_headder";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home () {
+const Index = () => {
   return (
-    <main>
+    <div>
+      <div>
+        <h1 className="text-4xl italic font-bold text-center tracking-widest">
+          JTBBS
+        </h1>
+        <p className="text-center">Java TypeScript BBS</p>
+      </div>
 
-    </main>
-  )
-}
+      <div className="mt-5">
+        <h4 className="text-center font-bold">掲示板一覧</h4>
+        <div className="flex justify-center p-2">
+          <div>
+            <BoardSelectBtn url="/java" boardName="java" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Index.getLayout = getLayout;
+
+export default Index;
