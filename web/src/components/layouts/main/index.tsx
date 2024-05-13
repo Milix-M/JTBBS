aef482f5448d805f7c457518872dd3b4856b82ca
Milix-M <tujikou12dh@gmail.com>
2024-05-13 21:45:24 +0900

    update index

--- web/src/components/layouts/main/index.tsx
+++ web/src/components/layouts/main/index.tsx
@@ -1,3 +1,5 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+import Link from "next/link";
 import { ReactElement } from "react";
 // import Header from "./Header/Header";
 
@@ -7,8 +9,31 @@ type LayoutProps = Required<{
 
 export const Layout = ({ children }: LayoutProps) => (
   <>
-    {/* <Header /> */}
-    <main className="w-full max-w-[45rem] mx-auto my-2">{children}</main>
+    <div className="flex">
+      <header className="border-r min-h-svh border-slate-600 max-w-xs w-full">
+        <div className="p-4">
+          <h1 className="text-4xl italic font-bold text-center tracking-widest">
+            JTBBS
+          </h1>
+          <div className="space-y-3 mt-5">
+            <div className="border-b p-2">
+              <Link href={"/"}>
+                <p className="text-center">Home</p>
+              </Link>
+            </div>
+            <p className="text-center mb-2">ボード一覧</p>
+            <div>
+              <p className="text-center">タイトル</p>
+              <p className="text-center">タイトル</p>
+            </div>
+          </div>
+          <div className="">
+            <ThemeToggle />
+          </div>
+        </div>
+      </header>
+      <main className="w-full mx-auto my-2">{children}</main>
+    </div>
   </>
 );
 
