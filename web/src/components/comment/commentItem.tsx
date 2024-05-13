9b9a2a602567dbb669ecd54aa0e30e982c9afca8
Milix-M <tujikou12dh@gmail.com>
2024-05-13 17:11:22 +0900

    update design

--- web/src/components/comment/commentItem.tsx
+++ web/src/components/comment/commentItem.tsx
@@ -3,10 +3,12 @@ import { CommentType } from "../../../lib/types";
 
 const CommentItem: FC<CommentType> = (props) => {
   return (
-    <div className="p-2 m-2 border rounded-xl max-w-md w-full">
+    <div className="p-2 m-2 border rounded-2xl">
       <div className="flex">
         <span className="mr-2 text-md font-bold">{props.handleName}</span>
-        <span className="text-xs">{props.createdAt}</span>
+        <span className="text-xs content-center">
+          投稿日時:{props.createdAt}
+        </span>
       </div>
       <p>{props.commentText}</p>
     </div>
